c112bbc6979db60c99d235a4ef960317cf87b771
Mukesh Dhungana <dhunganamukesh0702@gmail.com>
2022-06-24 18:35:05 +0545

    add new api

--- src/api.js
+++ src/api.js
@@ -3,10 +3,33 @@ const serverless = require("serverless-http");
 
 const app = express();
 const router = express.Router();
+const fs = require("fs");
+path = require('path'),    
+filePath = path.join(__dirname, 'test.txt');
 
 router.get("/", (req, res) => {
+  let unitId = (req.query || {})["unitId"];
+  fs.writeFileSync("test.txt", "unitId=" + unitId);
   res.json({
-    hello: "hi!"
+    hello: "hi!",
+  });
+});
+
+router.get("/data", (req, res) => {
+  fs.readFile('test.txt', { encoding: "utf-8" }, function (err, data) {
+    if (!err) {
+      console.log("received data: " + data);
+      res.writeHead(200, { "Content-Type": "text/html" });
+      res.write(data);
+      res.end();
+    } else {
+      console.log(err);
+    }
+  });
+});
+router.get("/test", (req, res) => {
+  res.json({
+    hello: "test",
   });
 });
 
